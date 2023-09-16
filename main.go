0333c218c9dbf297e77b30532d9e13e5a8b7359f
Eugene Chao <epchao@berkeley.edu>
2023-09-15 23:07:13 -0700

    Comment unfamiliar functionality

--- main.go
+++ main.go
@@ -7,14 +7,20 @@ import (
 	"net/http"
 )
 
+// Defining our "Dog" data schema
 type Dog struct {
   Name string
   Breed string
 }
 
+// main() function is automatically called and must
+// be apart of some package for execution
 func main() {
   fmt.Println("Hosting a server on localhost:8080")
 
+  // := denotes assignment
+  // We are using Golang's templates and using a dogs object to
+  // populate the template with data
   greet := func (write http.ResponseWriter, request *http.Request) {
     tmpl := template.Must(template.ParseFiles("index.html"))
     dogs := map[string][]Dog {
@@ -30,7 +36,9 @@ func main() {
     tmpl.Execute(write, dogs)
   }
 
+  // greet() will be run when a user loads the root URL
   http.HandleFunc("/", greet)
 
+  // fail-safe default and provide the server
   log.Fatal(http.ListenAndServe(":8080", nil))
 }
