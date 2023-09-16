f94b9e181ef168d26ab6b011b107842f6a11e006
Eugene Chao <epchao@berkeley.edu>
2023-09-15 23:52:12 -0700

    Modify main.go to keep dogs in list until app shutdown

--- main.go
+++ main.go
@@ -19,21 +19,22 @@ type Dog struct {
 func main() {
   fmt.Println("Hosting a server on localhost:8080")
 
+  dogs := map[string][]Dog {
+   "Dogs": {
+      { Name: "Bear", Breed: "Maltese" },
+      { Name: "Lily", Breed: "Chihuahua" },
+      { Name: "Coco", Breed: "Corgi" },
+      { Name: "Mochi", Breed: "Chow Chow" },
+      { Name: "Oliver", Breed: "Golden Doodle" },
+      { Name: "Yuki", Breed: "Akita" },
+    },
+  }
+
   // := denotes assignment
   // We are using Golang's templates and using a dogs object to
   // populate the template with data
   greet := func (write http.ResponseWriter, request *http.Request) {
     tmpl := template.Must(template.ParseFiles("index.html"))
-    dogs := map[string][]Dog {
-      "Dogs": {
-        { Name: "Bear", Breed: "Maltese" },
-        { Name: "Lily", Breed: "Chihuahua" },
-        { Name: "Coco", Breed: "Corgi" },
-        { Name: "Mochi", Breed: "Chow Chow" },
-        { Name: "Oliver", Breed: "Golden Doodle" },
-        { Name: "Yuki", Breed: "Akita" },
-      },
-    }
     tmpl.Execute(write, dogs)
   }
 
@@ -42,8 +43,11 @@ func main() {
     time.Sleep(1 * time.Second)
     name := request.PostFormValue("name")
     breed := request.PostFormValue("breed")
+    dog := Dog{Name: name, Breed: breed}
+
     tmpl := template.Must(template.ParseFiles("index.html"))
-    tmpl.ExecuteTemplate(write, "dog-list-element", Dog{Name: name, Breed: breed})
+    tmpl.ExecuteTemplate(write, "dog-list-element", dog)
+    dogs["Dogs"] = append(dogs["Dogs"], dog)
   }
 
   // greet() will be run when a user loads the root URL
